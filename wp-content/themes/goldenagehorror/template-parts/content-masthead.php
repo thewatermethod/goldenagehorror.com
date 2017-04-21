63a1d87369b5c4ca2acc5070c8bdb00761948678
Matt Bevilacqua <mbevilac@providence.edu>
2017-04-21 16:24:54 -0400

    google svg, additional mobile stylins'

--- wp-content/themes/goldenagehorror/template-parts/content-masthead.php
+++ wp-content/themes/goldenagehorror/template-parts/content-masthead.php
@@ -17,7 +17,7 @@
 	<?php if ($itunes_link != '' ): ?><a href="<?php echo $itunes_link;?>"><img src="<?php echo get_template_directory_uri(); ?>/svg/itunes.svg" alt="Get it on itunes"></a><?php endif; ?>
 	
 	<?php $google_play_link = get_theme_mod('google_play'); ?>
-	<?php if ($google_play_link != '' ): ?><a href="<?php echo $google_play_link; ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/google-play-badge.png" alt="Get it on Google Play"></a><?php endif; ?>
+	<?php if ($google_play_link != '' ): ?><a href="<?php echo $google_play_link; ?>"><img src="<?php echo get_template_directory_uri(); ?>/svg/google.svg" alt="Get it on Google Play"></a><?php endif; ?>
 
 	<?php $stitcher_link = get_theme_mod('stitcher'); ?>
 	<?php if ($stitcher_link != '' ): ?><a href="<?php echo $stitcher_link; ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/stitcher_button_small.png" alt="Available on Stitcher"></a><?php endif; ?>
