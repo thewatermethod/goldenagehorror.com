7cf30fdb9fd034d51ccde15b89df33e905a2a92e
Matt Bevilacqua <mbevilac@providence.edu>
2017-06-29 09:38:14 -0400

    adding lazy loading to theme

--- wp-content/themes/goldenagehorror/inc/catch-that-image.php
+++ wp-content/themes/goldenagehorror/inc/catch-that-image.php
@@ -20,10 +20,17 @@ function goldeagehorror_catch_that_image( $post_content ) {
 }	
 
 
-function blazy_image_tag( $html, $id, $alt, $title)  {
-	
-	return str_replace( 'src="', 'src="'. get_stylesheet_directory_uri() .'/img/static.gif" data-src="', $html );
-	
+// Here is the filter for activating lazy load
+
+if( !is_home() || !is_front_page() ) : add_filter('the_content', 'filter_lazyload'); endif;
+
+function filter_lazyload($content) {
+    return preg_replace_callback('/(<\s*img[^>]+)(src\s*=\s*"[^"]+")([^>]+>)/i', 'preg_lazyload', $content);
 }
 
-add_filter('get_image_tag', 'blazy_image_tag', 10, 4);
+function preg_lazyload($img_match) {
+ 	$img_replace = $img_match[1] . 'src="' . get_stylesheet_directory_uri() . '/img/static.gif" data-src' . substr($img_match[2], 3) . $img_match[3];
+    $img_replace = preg_replace('/class\s*=\s*"/i', 'class="b-lazy ', $img_replace);
+    $img_replace .= '<noscript>' . $img_match[0] . '</noscript>';
+    return $img_replace;
+}
